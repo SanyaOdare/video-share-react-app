23c9b6fbb0b2693a8484934a04ebec2ec5c8167b
SanyaOdare <sanyaodare@gmail.com>
2022-08-28 22:56:47 +0100

    add Videos component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,26 +4,39 @@ import { categories } from '../utils/constants';
 const selectedCategory = 'New';
 
 const Sidebar = () => (
-    <Stack
-      direction='row'
-      sx={{
-        overflowY: 'auto',
-        height: { sx: 'auto', md: '95%' },
-        flexDirection: { md: 'column' },
-      }}
-    >
-      {categories.map((category) => (
-        <button
-          className='category-btn'
+  <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+  >
+    {categories.map((category) => (
+      <button
+        className="category-btn"
+        style={{
+          background: category.name === selectedCategory && "#FC1503",
+          color: "white",
+        }}
+        key={category.name}
+      >
+        <span
           style={{
-            background: category.name === selectedCategory && '#FC1503'
+            color: category.name === selectedCategory ? "white" : "red",
+            marginRight: "15px",
           }}
         >
-          <span>{category.icon}</span>
-          <span>{category.name}</span>
-        </button>
-      ))}
-    </Stack>
-  )
+          {category.icon}
+        </span>
+        <span
+          style={{ opacity: category.name === selectedCategory ? '1' : '0.8'}}
+        >
+          {category.name}
+        </span>
+      </button>
+    ))}
+  </Stack>
+);
 
 export default Sidebar;
